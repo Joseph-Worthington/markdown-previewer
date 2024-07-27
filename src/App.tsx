e99a2670a02b437d3702a92a8d3e163baf7d5c12
Joe Worthington <joeyworthington@hotmail.co.uk>
2024-07-27 15:31:21 +0100

    add dark mode and style dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Editor from './components/Editor';
 import Preview from './components/Preview';
 import DefaultText from './components/DefaultText';
 
 function App() {
   const [value, setValue] = React.useState<string>(DefaultText);
+  const [dark, setDark] = useState(false)
     
   const onChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
       setValue(event.target.value);
   }
+  const toggleTheme = () => {
+    setDark(!dark)
+    document.body.classList.toggle('dark')
+  }
+  if(document.body.classList.contains('dark') && !dark){
+    setDark(true)
+  }
+  
 
   return (
-    <div className="App p-10 bg-slate-500 flex flex-col items-center min-h-screen">
+    <div className="App p-10 bg-slate-500 dark:bg-slate-900 flex flex-col items-center min-h-screen">
+      <button onClick={toggleTheme} className="bg-blue-500 text-white p-2 rounded-md">Toggle Theme</button>
       <h1 className="text-3xl font-bold underline text-white"> Markdown Previewer </h1>
       <div className=" flex justify-evenly gap-10 py-10 xl:p-10 w-[80%] flex-wrap xl:flex-nowrap">
       <Editor onChange={onChange} value={value}/>
--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
 interface EditorProps {
-value: string;
+  value: string;
   onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
 }
 
 const Editor: React.FC<EditorProps> = ({ value, onChange }) => {
 
   return (
-    <div className='bg-slate-200 border-2 border-solid border-black p-10 w-full xl:w-[40%] min-h-96'>
+    <div className='bg-slate-200 dark:bg-slate-700 border-2 border-solid border-black p-10 w-full xl:w-[40%] min-h-96'>
       <h2>Editor</h2>
-      <textarea id="editor" onChange={onChange} value={value} className='w-full min-h-[80%] p-5'/>
+      <textarea id="editor" onChange={onChange} value={value} className='w-full min-h-[80%] p-5 dark:bg-gray-800 dark:text-slate-50'/>
     </div>
   );
 }
